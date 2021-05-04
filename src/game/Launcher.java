12684d69c0d01d7e6b46c0257fa66d10352566ac
tienbaovuong <tienbaoams@gmail.com>
2021-05-04 20:30:42 +0700

    update tiep

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -3,7 +3,7 @@ package game;
 public class Launcher {
     
     public static void main(String[] args) {
-        Game game = new Game("Title Game!", 800, 608);
+        Game game = new Game("RPG GAME!", 800, 608);
         game.start();
     }
 }
