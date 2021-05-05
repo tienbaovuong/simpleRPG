ad53129b506b21fb33c38e5b8710c01e004bbc60
galaxysomi <a1tp.quang@gmail.com>
2021-05-05 11:31:14 +0700

    update : AOE music

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -1,10 +1,25 @@
 package game;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
 
 public class Launcher {
     
     public static void main(String[] args) {
         Game game = new Game("RPG GAME!", 800, 608);
         game.start();
+        try {
+            FileInputStream fileInputStream = new FileInputStream("hello.mp3");
+            Player player = new Player(fileInputStream);
+            player.play();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JavaLayerException e) {
+            e.printStackTrace();
+        }
     }
 }
     
