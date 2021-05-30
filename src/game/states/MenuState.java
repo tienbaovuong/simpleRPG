7ff9a1e80b6745361dc1e28e5d2373bdb7f09ed9
tienbaovuong <tienbaoams@gmail.com>
2021-05-30 14:34:14 +0700

    Revert "Revert "add bonus score after complete a stage""
    This reverts commit f1d7fe89f88cc857f62959040ea0aca199af32bf.

--- src/game/states/MenuState.java
+++ src/game/states/MenuState.java
@@ -48,7 +48,7 @@ public class MenuState extends State {
         g.drawImage(Assets.logoMenu,225,112,371,76,null);
 
         uiManager.render(g);
-        new ScoreBoard(0,0, 0);
+        new ScoreBoard(handler);
         long highestScore = ScoreBoard.getHighestScoreFromFile();
         g.setColor(Color.black);
         g.setFont(new Font("TimesRoman", Font.PLAIN, 40));
